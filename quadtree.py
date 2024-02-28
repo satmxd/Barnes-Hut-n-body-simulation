00c32ddb82f838d9f1afbfc5c561de5c4b4b8294
satmxd <sat.mdyi@gmail.com>
2024-02-28 16:00:54 +0530

    mass working

--- quadtree.py
+++ quadtree.py
@@ -1,7 +1,10 @@
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.mass = 1
 
 class Rectangle:
     def __init__(self, x, y, w, h):
@@ -9,19 +12,35 @@ class Rectangle:
         self.y = y
         self.w = w  # Distance from center to boundary - Actual width/2
         self.h = h
+        self.points = []
+        self.mass = 0
 
     def contains(self, point):
-        contain =  (point.x >= self.x-self.w and point.x <= self.x + self.w and point.y >= self.y - self.h and point.y <= self.y + self.h)  # Check if Rectangle contains point
-        return contain
-    def draw(self, canvas):
+        return (point.x >= self.x-self.w and point.x < self.x + self.w and point.y >= self.y - self.h and point.y < self.y + self.h)  # Check if Rectangle contains point
+
+    def intersects(self, range):
+        return not(range.x - range.w > self.x + self.w or
+                   range.x + range.w < self.x - self.w or
+                   range.y - range.h > self.y + self.h or
+                   range.y + range.h < self.y - self.h)
+    def draw(self, canvas, depth, mass):
         canvas.create_rectangle(self.x-self.w,self.y-self.h, self.x+self.w, self.y+self.h, outline = 'white' , width = 1)
+        canvas.create_text(self.x-20, self.y, text = depth, fill = 'white')
+        canvas.create_text(self.x+20, self.y, text = mass, fill = 'white')
+
 
 class QuadTree:
-    def __init__(self, boundary, n):
+    def __init__(self, boundary, n, depth):
         self.boundary = boundary  # Rectangle
         self.capacity = n
         self.points = []
         self.subdivided = False
+        self.depth = depth
+        self.count = 0
+        self.com_x = 0
+        self.com_y = 0
+        self.mass = 0
+        self.parent = None
 
     def subdivide(self):
         x = self.boundary.x
@@ -34,31 +53,73 @@ class QuadTree:
         bl = Rectangle(x - w / 2, y + h / 2, w / 2, h / 2)
         br = Rectangle(x + w / 2, y + h / 2, w / 2, h / 2)
         # Define each quadrant as another QuadTree
-        self.topright = QuadTree(tr, self.capacity)
-        self.topleft = QuadTree(tl, self.capacity)
-        self.bottomleft = QuadTree(bl, self.capacity)
-        self.bottomright = QuadTree(br, self.capacity)
+        self.topright = QuadTree(tr, self.capacity, self.depth+'B')
+        self.topleft = QuadTree(tl, self.capacity, self.depth+'A')
+        self.bottomleft = QuadTree(bl, self.capacity, self.depth+'C')
+        self.bottomright = QuadTree(br, self.capacity, self.depth+'D')
+        self.topright.parent = self.topleft.parent = self.bottomleft.parent = self.bottomright.parent = self
+        for i in range(len(self.points)):
+            self.topright.insert(self.points[i])
+            self.topleft.insert(self.points[i])
+            self.bottomleft.insert(self.points[i])
+            self.bottomright.insert(self.points[i])
         self.subdivided = True
+
 
     def insert(self, point):
         if not self.boundary.contains(point):
             return False
-        elif len(self.points) < self.capacity:
+        if len(self.points) < self.capacity:
             self.points.append(point)
             return True
-        elif not self.subdivided:
-            self.subdivide()
-            return True
-        return (self.topleft.insert(point) or
-                self.topright.insert(point) or
-                self.bottomleft.insert(point) or
-                self.bottomright.insert(point))
+        else:
+            if not self.subdivided:
+                self.subdivide()
+            return (self.topright.insert(point) or
+                    self.topleft.insert(point) or
+                    self.bottomright.insert(point) or
+                    self.bottomleft.insert(point))
+
+    def query(self, range, found_points):
+        if not self.boundary.intersects(range):
+            return
+        for point in self.points:
+            if range.contains(point):
+                found_points.append(point)
+
+        if self.subdivided:
+            self.topright.query(range, found_points)
+            self.topleft.query(range, found_points)
+            self.bottomright.query(range, found_points)
+            self.bottomleft.query(range, found_points)
+        return found_points
+
+    # def calculate_mass(self):
+    #     #self.mass = len(self.query(self.boundary, []))
+    #     if self.subdivided:
+    #         trm = self.topright.mass = len(self.query(self.topright.boundary, []))
+    #         tlm = self.topleft.mass = len(self.query(self.topleft.boundary, []))
+    #         brm = self.bottomright.mass = len(self.query(self.bottomright.boundary, []))
+    #         blm = self.bottomleft.mass = len(self.query(self.bottomleft.boundary, []))
+    #         self.mass = trm+tlm+brm+blm
+    #     else:
+    #         print(self.points)
+    #         #self.mass = len(self.points)
+
+    def __len__(self):
+        mass = len(self.points)
+        if self.subdivided:
+            mass = len(self.topright) + len(self.topleft) + len(self.bottomright) + len(self.bottomleft)
+        return mass
+
 
     def display(self, canvas):
-        self.boundary.draw(canvas)
-        # canvas.create_rectangle(self.boundary.x-self.boundary.w,self.boundary.y-self.boundary.h, self.boundary.w*2, self.boundary.h*2, outline = 'black' , width = 2)
+        self.boundary.draw(canvas, self.depth, len(self))
+        #print(self.depth, self.points)
         if self.subdivided:
             self.topleft.display(canvas)
             self.topright.display(canvas)
             self.bottomleft.display(canvas)
             self.bottomright.display(canvas)
+            #print(f'tl: {len(self.topleft)},  tr: {len(self.topright)}, bl: {len(self.bottomleft)}, br: {len(self.bottomright)}')
+
--- simulation.py
+++ simulation.py
@@ -6,30 +6,48 @@ from numpy import random as rd
 
 
 start = time.time()
-width, height = 800, 800
+width, height = 700, 700
 root = tk.Tk()
 canvas = tk.Canvas(root, width = width, height = height, background='black')
 canvas.pack()
+colours = ["cyan",
+           "magenta",
+           "red",
+           "blue",
+           "gray"]
 
 rect = Rectangle(width/2, height/2, width/2, height/2)
-quadtree = QuadTree(rect, 4)
 
-# for i in range(1000):
+points = 50
+
+quadtree = QuadTree(rect,  4, '')
+
+
+# for i in range(10000):
 #     x, y = random.randrange(0, width), random.randrange(0, height)
-#     quadtree.insert(Point(x, y))
+#     quadtree.insert(Point(x, y, 1))
 #
 #     canvas.create_oval(x, y, x + 4, y + 4, fill='white')
-points = 5000
 gaussian =rd.normal(width/2, 100, size = (points, 2))
-
+p = []
 for i in range(points):
     x, y = gaussian[i][0], gaussian[i][1]
-    canvas.create_oval(x, y, x + 4, y + 4, fill='white')
-    quadtree.insert(Point(x, y))
+    canvas.create_oval(x, y, x + 15, y + 15, fill=random.choice(colours))
+    # p.append(Point(x,y))
+    quadtree.insert(Point(x,y))
 
+# print(quadtree.query(quadtree.boundary, []))
+# range = Rectangle(300, 300, 120, 120)
+# canvas.create_rectangle(range.x-range.w,range.y-range.h, range.x+range.w, range.y+range.h, outline = 'green' , width = 1)
+# pts = []
+# quadtree.query(range, pts)
+# print(len(pts))
+# for p in pts:
+#     canvas.create_oval(p.x, p.y, p.x + 5, p.y + 5, fill='green')
+
+print('total points = ', len(quadtree))
 quadtree.display(canvas)
-
 end = time.time()
-print(end - start)
+print(f'time: {end - start}')
 tk.mainloop()
 
