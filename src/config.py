fc5809716ffdd84289276184255c5f87f40ed927
Satvik <122893966+satmxd@users.noreply.github.com>
2024-04-14 12:43:05 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,9 +5,9 @@ theta = 0.45
 G = 0.82
 dt = 15
 floatcutoff = 0.01
-# r = sb.color_palette('mako', 29)
-# colorrange = r.as_hex()[2:]
-colorrange = ['#3d0f71', '#4a1079', '#56147d', '#621980', '#6d1d81', '#792282', '#842681', '#912b81', '#9c2e7f', '#aa337d', '#b73779', '#c23b75', '#cf4070', '#d9466b', '#e44f64', '#ec5860', '#f3655c', '#f7725c', '#fa815f', '#fc8e64', '#fe9d6c', '#feaa74', '#feb97f', '#fec68a', '#fed597', '#fde2a3', '#fcf0b2']
+r = sb.color_palette('mako', 29)
+colorrange = r.as_hex()[2:]
+#colorrange = ['#3d0f71', '#4a1079', '#56147d', '#621980', '#6d1d81', '#792282', '#842681', '#912b81', '#9c2e7f', '#aa337d', '#b73779', '#c23b75', '#cf4070', '#d9466b', '#e44f64', '#ec5860', '#f3655c', '#f7725c', '#fa815f', '#fc8e64', '#fe9d6c', '#feaa74', '#feb97f', '#fec68a', '#fed597', '#fde2a3', '#fcf0b2']
 
 
 
